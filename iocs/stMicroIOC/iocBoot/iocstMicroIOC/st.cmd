03a148420ae965d99a2e140560ffe0fa59dc30ab
redomitro <rea.domitrovic@irb.hr>
2026-07-22 13:32:12 +0200

    built IOC

--- iocs/stMicroIOC/iocBoot/iocstMicroIOC/st.cmd
+++ iocs/stMicroIOC/iocBoot/iocstMicroIOC/st.cmd
@@ -15,10 +15,20 @@ cd "${TOP}"
 dbLoadDatabase "dbd/stMicroIOC.dbd"
 stMicroIOC_registerRecordDeviceDriver pdbbase
 
+drvAsynSPIConfigure("L0", "/dev/spidev0.0", 0, 0, 1)
+ihm02a1CreateController("uProbChopper", "L0", 2, 10, 1000, 0)
+
 ## Load record instances
 #dbLoadRecords("db/stMicroIOC.db","user=rea")
+dbLoadRecords("db/motor.db","SYS=UPROB, SUB=Chopper")
 
 cd "${TOP}/iocBoot/${IOC}"
+
+asynSetTraceIOMask("L0", 0, 2)
+asynSetTraceMask("L0", 0, ERROR|WARNING)
+asynSetTraceIOMask("uProbChopper", 0, 2)
+asynSetTraceMask("uProbChopper", 0, ERROR|WARNING)
+
 iocInit
 
 ## Start any sequence programs
